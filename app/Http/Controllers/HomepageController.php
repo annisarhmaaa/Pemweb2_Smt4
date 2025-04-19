402e30457b71846c7cd4e7b517e880e6b16c1535
Annisa Nur Ramadhani <annisanurramadhani93@gmail.com>
2025-04-19 20:38:03 +0700

    Database-Menambahkan tabel product

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Categories;
+
+class HomepageController extends Controller
+
+{
+    public function index()
+    {
+        $categories = Categories::all();
+
+        return view('web.homepage',[
+        'categories' => $categories,
+        ]);
+ }
+    public function products()
+    {
+        return view('web.products');
+    }
+    public function producT($slug){
+        return view('web.product', ['slug' => $slug]);
+    }
+    public function categories()
+    {
+        return view('web.categories');
+    }
+    public function category($slug)
+    {
+        return view('web.category_by_slug', ['slug' => $slug]);
+    }
+    public function cart()
+    {
+        return view('web.cart');
+    }
+    public function checkout()
+    {
+        return view('web.checkout');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,37 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\HomepageController;
 
-Route::get('/', function () {
-    return view('web.homepage');
-});
-
-Route::get('/products', function () {
-    return view('web.products');
-});
-
-Route::get('/categories', function () {
-    return view('web.categories');
-});
-
-Route::get('/reviews', function () {
-    return view('web.reviews');
-});
-
-Route::get('/promos', function () {
-    return view('web.promos');
-});
-
-Route::get('/orders', function () {
-    return view('web.orders');
-});
-
-Route::get('/payments', function () {
-    return view('web.payments');
-});
-
-Route::get('/products/{id}', function ($id) {
-    return "Ini Detail produk dengan ID: $id";
-});
-
+Route::get('/', [HomepageController::class, 'index']);
+Route::get('products', [HomepageController::class, 'products']);
+Route::get('product/{slug}', [HomepageController::class, 'product']);
+Route::get('categories',[HomepageController::class, 'categories']);
+Route::get('category/{slug}', [HomepageController::class, 'category']);
+Route::get('cart', [HomepageController::class, 'cart']);
+Route::get('checkout', [HomepageController::class, 'checkout']);
 require __DIR__.'/auth.php';
